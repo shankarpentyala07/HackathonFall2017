72da5d5fe2a84eea485dcfe20b8244abb2bf5e4a
shankar <shankarmscs@gmail.com>
2017-11-12 21:10:17 -0600

    commit

---
+++ Source/filter-resume-Project-master/app.js
@@ -0,0 +1,186 @@
+var express = require('express');
+var SparkPost = require('sparkpost');
+var sp = new SparkPost('*************');
+var port = process.env.PORT || 3000
+var bodyParser = require('body-parser');
+var path = require('path');
+var expressValidator = require('express-validator');
+var mongojs = require('mongojs')
+var mongodb = require('mongodb')
+var collections = ["users", "blog", "comments", "property", "images", "notification", "bookmark", "messages","timetable", "timetablecategory", "timetablequestion", "collection1", "skills", "locations"]
+var db = mongojs('mongodb://root:secure@ds155695.mlab.com:55695/shankarp', collections)
+var app = express();
+var ObjectId = mongojs.ObjectId;
+var passport = require("passport")
+var JSFtp = require("jsftp");
+var fs = require('fs');
+var config = {
+  host: 'localhost',
+  port: 21,
+  user: 'root',
+  password: 'secure'
+}
+var Client = require('ftp');
+// var c = new Client();
+// c.connect(config);
+// View Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname)));
+// body parser middleware
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb',extended: false}));
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname)));
+
+var multiparty = require('connect-multiparty'),
+  multipartyMiddleware = multiparty();
+  app.use(multipartyMiddleware);
+    app.use(function(req, res, next){
+      fs.appendFile('logs.txt', req.path + "token:" + req.query.access_token+'', 
+        function(err){
+          next(); 
+        });
+  });
+
+app.get('/', function(req, res){       
+  db.collection1.find(function (err, resume) {
+    res.render("index.ejs", {message:"", resumecount: resume.length});
+  });
+});
+
+app.post('/getmatch', function(req, res){   
+  var skills = req.body.skills;
+  db.collection1.find(function (err, resumeContent) {
+    res.send(resumeContent);    
+  });
+});
+
+app.post('/upload', function(req, res){       
+  var file = req.files.file;
+  var filepath = file.path;
+  var fullname = req.body.fullname;
+  var email = req.body.email;
+  var timestamp = new Date().valueOf();
+  
+  var date = new Date();
+  var datetime = (date.getMonth()+1)+" / "+date.getDate()+" / "+date.getFullYear()+" at "+date.getHours()+":"+date.getMinutes();
+  let PDFParser = require("pdf2json");
+  let pdfParser = new PDFParser(this,1);
+  pdfParser.loadPDF(file.path);         
+
+  pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
+  pdfParser.on("pdfParser_dataReady", pdfData => {
+  var resumeContent = pdfParser.getRawTextContent();
+      var c = new Client();
+      c.on('ready', function() {
+        c.put(file.path, 'localhost'+timestamp+"-"+file.originalFilename, function(err) {
+          if (err) throw err;
+          c.end();
+        });
+      });
+
+     c.connect(config);
+      var resumeurl = 'http://'+timestamp+"-"+file.originalFilename;
+      var newResume = {
+        fullname: fullname,
+        email: email,
+        filename: file.originalFilename,
+        resume: resumeurl,
+        timestamp: timestamp,
+        datetime: datetime,
+        resumetext: resumeContent
+      }
+      db.collection1.insert(newResume, function(err, result){
+        if(err){console.log(err);}
+         db.collection1.find(function (err, resume) {
+            res.render("index.ejs", {message:"Thank you for submitting your Resume. We will review your application and contact you shortly.", resumecount: resume.length});
+          })        
+        });
+      });   
+});
+
+app.post('/upload2', function(req, res){       
+  var file = req.files.file;
+  var filepath = file.path;
+  var timestamp = new Date().valueOf();
+   
+  var c = new Client();
+    c.on('ready', function() {
+      c.put(file.path, 'htdocs/public_html/career/'+file.originalFilename, function(err) {
+        if (err) throw err;
+         c.end();
+        });
+   });
+   c.connect(config);
+   res.redirect("/");     
+});
+
+app.post('/addloc', function(req, res){
+var date = new Date();
+var datetime = date.getMonth()+1+"/"+date.getDate()+"/"+date.getFullYear()+" at "+date.getHours()+":"+date.getMinutes();
+var long = req.body.long;
+var lat = req.body.lat;
+var whatdone = req.body.task;
+console.log(long);
+var lat_1 = Number(lat)-0.000203;
+var lat_2 = Number(lat)+0.000203;
+var long_1 = Number(long)-0.00070989999;
+var long_2 = Number(long)+0.00070989999;     
+db.locations.findOne({
+       $and : [
+          { $and : [ { lat : { $gt: lat_1} }, { lat : { $lt: lat_2} } ] },
+          { $and : [ { long: { $gt: long_1} }, { long : { $lt: long_2} } ] }
+      ]
+      }, function(err, location) {
+      if (!location) {
+        var newLoc = {
+          visittime: 1,
+          re_c: 0,
+          tt_c: 0,
+          bb_c: 0,
+          rs_c: 1,
+          mm_c: 0,
+          re_task: "",
+          tt_task: "",
+          bb_task: "",
+          rs_task: whatdone+" ("+datetime+")",
+          mm_task: "",
+          long: Number(long),
+          lat: Number(lat)
+        }
+        db.locations.insert(newLoc, function(err, result){
+        if(err){console.log(err);}
+        res.send("INSERTED");
+        });
+      }else {
+        var count = location.visittime+1;
+        var cc = location.rs_c+1;
+        whatdone = whatdone+" ("+datetime+"),"+location.rs_task;
+        db.locations.update({_id: location._id},{$set : {"visittime": count, "rs_c": cc, "rs_task": whatdone}},{upsert:true,multi:false});
+        res.send("UPDATED: "+count);
+      }
+  });
+});
+
+// This method is ADMIN SIDE METHOD
+app.get('/visitormap', function(req, res){  
+var pageno = Number(0);  
+  db.locations.find(function (err, locs) {
+      res.render("visitormap.ejs",{locs: locs});
+  })
+});
+app.post('/searchLocation', function(req, res) {
+  var id = req.body.id;
+  var ObjectID = require('mongodb').ObjectID;
+  var o_id = new ObjectID(id);
+   db.locations.findOne({ '_id': o_id}, function (err, location) {
+    res.send(location);
+  });
+});
+app.listen(port, function() {
+  console.log('Listening on port ' + port)
+})
+
+
---
+++ Source/filter-resume-Project-master/public/js/index.js
@@ -0,0 +1,221 @@
+var task = "RS-HOME";
+
+$( document ).ready(function() {
+    $("#admin-awsdivout").hide();
+    $("#awsdiv").hide();
+    $("#resumeselector").show();
+    loadResume();    
+});
+function loadResume(){
+ var skills = $(".remove").text(); 
+    var count = 0, count1 = 0;
+    $("#resumeappendout").html("<table width='100%'><thead><tr><th>Name</th><th>Email</th><th>Resume URL</th><th>Date</th><th>Approve Status</th><th>Action Status</th></tr></thead><tbody id='resumeappend'></tbody><table>");
+    $("#load").html('<img src="images/load.gif">');
+    var result = skills.split(" "); 
+    $.post( "/getmatch", { skills: skills})
+    .done(function(resume){
+        setTimeout(function(){
+            var l = resume.length;
+            for (i = 0; i < l; i++) {
+                 $("#resumeappend").append('<tr class="nofilter"><td>'+resume[i].fullname+'</td><td>'+resume[i].email+'</td>'+
+                '<td><a href="'+resume[i].resume+'" target="_blank"><i class="fa fa-file-pdf-o fa-1x"> Resume</i></a></td>'+
+                '<td>'+resume[i].datetime+'</td><td> <img src="images/warn.jpg" width="12" height="12"> No Filter</td>'+
+                '<td> <img src="images/warn.jpg" width="12" height="12"> No Filter</td></tr>');
+                count1++;
+            }
+        $("#resultcount").text("TOTAL RESUME IN SYSTEM = "+count1);
+        $("#load").html('');
+        },2500);
+    })
+}
+
+
+$(document).on("click",".allresume",function() {
+    $("#admin-awsdivout").hide();
+    $("#awsdiv").hide();
+    $("#resumeselector").show();
+    task = "RS-ALLRESUME";
+    updateLocation();
+    longLatCurrent();
+
+});
+
+$(document).on("click",".admin",function() {
+    var person = prompt("Please enter admin password", "");
+    task = "RS-ADMIN";
+    updateLocation();
+    longLatCurrent();
+    if (person == "sysy") {
+        $("#admin-awsdivout").show();
+        $("#resumeselector").hide();
+        $("#awsdiv").hide();
+    }else{
+        alert("WRONG PASSWORD")
+    }
+});
+
+
+$(document).on("click",".upload",function() {
+    task = "RS-UPLOAD";
+    updateLocation();
+    longLatCurrent();
+    $("#admin-awsdivout").hide();
+    $("#awsdiv").show();
+    $("#resumeselector").hide();
+});
+
+
+
+$(document).on("change",".select",function() { 
+    var input = $(this).val();
+    task = "RS-SKILL-"+input;
+    updateLocation();
+    longLatCurrent();
+    var skillsdiv = $("#skillsdiv").html();
+    skillsdiv = "<span class='remove' id='"+input+"'>"+input+" <img src='images/cross.jpg' width='12' height='12'><span>";
+    if(input!="default"){
+        $("#skillsdiv").append(skillsdiv); 
+    }  
+    $("#go").click();
+});
+$(document).on("click","#go", function(){
+    var skills = $(".remove").text(); 
+    var count = 0, count1 = 0;
+    $("#resumeappendout").html("<table width='100%'><thead><tr><th>Name</th><th>Email</th><th>Resume URL</th><th>Date</th><th>Approve Status</th><th>Action Status</th></tr></thead><tbody id='resumeappend'></tbody><table>");
+    
+    $("#load").html('<img src="images/load.gif">');
+    var result = skills.split(" "); 
+    $.post( "/getmatch", { skills: skills})
+    .done(function(resume){
+        setTimeout(function(){
+        var l = resume.length;
+        for (i = 0; i < l; i++) {
+          var con = resume[i].resumetext.toLowerCase();
+          var status = false;
+          for (j = 0; j < result.length; j++) {
+            var skill = result[j];
+            if(con.includes(skill)){
+                status = true;
+            }else{
+                status = false; break;
+            }
+          }
+          if(status==true){
+            count++;
+            $("#resumeappend").append('<tr class="approved"><td>'+resume[i].fullname+'</td><td>'+resume[i].email+'</td>'+
+                '<td><a href="'+resume[i].resume+'" target="_blank"><i class="fa fa-file-pdf-o fa-1x"> Resume</i></a></td>'+
+                '<td>'+resume[i].datetime+'</td><td> <img src="images/done.jpg" width="12" height="12"> Resume Approved</td><td>SEND APPROVE <i class="fa fa-envelope-open-o"></td></tr>');
+            }else{
+                count1++;
+                $("#resumeappend").append('<tr class="rejected"><td>'+resume[i].fullname+'</td><td>'+resume[i].email+'</td>'+
+                '<td><a href="'+resume[i].resume+'" target="_blank"><i class="fa fa-file-pdf-o fa-1x"> Resume</i></a></td>'+
+                '<td>'+resume[i].datetime+'</td><td> <img src="images/cross.jpg" width="12" height="12"> Resume Rejected</td><td>SEND - REJECT <i class="fa fa-envelope-open-o"></td></tr>');
+            }
+         }
+        $("#resultcount").text(count+" RESUME APPROVED | "+count1+" RESUME REJECTED");
+        $("#load").html('');
+     },100);
+    })
+})
+
+ 
+$(document).on("click",".remove",function() { 
+    $(this).remove();
+    $("#go").click();
+});
+
+$(document).on("click","#checkpdf",function() { 
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var file = $("#file").val();
+    var ext = file.split('.').pop();
+    var count = 0;
+    // alert(name+", "+email+", "+ext)
+    if(name=="") $("#name-msg").html(" <img src='images/cross.jpg' width='12' height='12'> Invalid Name");
+    else { count++; $("#name-msg").html(" <img src='images/done.jpg' width='12' height='12'> DONE");}
+
+    if(!validateForm(email)) $("#email-msg").html(" <img src='images/cross.jpg' width='12' height='12'> Invalid Email");
+    else { count++; $("#email-msg").html(" <img src='images/done.jpg' width='12' height='12'> DONE");}
+    
+    if(ext=="pdf") {count++; $("#file-msg").html(" <img src='images/done.jpg' width='12' height='12'> DONE");}
+    else { $("#file-msg").html(" <img src='images/cross.jpg' width='12' height='12'> - Invalid File");}
+
+    if(count==3){
+        $("#finalsubmit").click();
+    }else{
+    }
+
+});
+
+$(document).on("keyup, change","#name,#email, #file",function() {
+   var name = $("#name").val();
+    var email = $("#email").val();
+    var file = $("#file").val();
+    var ext = file.split('.').pop();
+    // alert(name+", "+email+", "+ext)
+    if(name=="") $("#name-msg").html(" <img src='images/cross.jpg' width='12' height='12'> Invalid Name");
+    else $("#name-msg").html(" <img src='images/done.jpg' width='12' height='12'> DONE");
+
+    if(!validateForm(email)) $("#email-msg").html(" <img src='images/cross.jpg' width='12' height='12'> Invalid Email");
+    else $("#email-msg").html(" <img src='images/done.jpg' width='12' height='12'> DONE");
+    
+    if(ext=="pdf") { $("#file-msg").html(" <img src='images/done.jpg' width='12' height='12'> DONE");}
+    else { $("#file-msg").html(" <img src='images/cross.jpg' width='12' height='12'> - Invalid File");}
+});
+function validateForm(email) {
+    var x = email;
+    var atpos = x.indexOf("@");
+    var dotpos = x.lastIndexOf(".");
+    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
+        return false;
+    }else{ return true;}
+}
+var demoLong = 0;
+var demoLat = 0;
+  function longLatCurrent()
+     {
+        if( navigator.geolocation )
+        {
+           // Call getCurrentPosition with success and failure callbacks
+           navigator.geolocation.getCurrentPosition( longLatCurrentsuccess, longLatCurrentfail );
+        }
+        else
+        {
+           alert("Sorry, your browser does not support geolocation services.");
+        }
+     }
+
+     function longLatCurrentsuccess(position)
+     {
+         var long = position.coords.longitude;
+         var lat = position.coords.latitude;
+         demoLong = long;
+         demoLat = lat;
+         $.post( "/addloc", { long: long, lat: lat, task: task})
+            .done(function( property ) {  
+        });
+     }
+
+     function longLatCurrentfail()
+     {
+        demoLong = '-73.824582';
+        demoLat = '40.670298';
+        $.post( "/addloc", { long: '-73.824582', lat: '40.670298', task: task})
+            .done(function( property ) {  
+        });     
+    }
+$(document).ready(function(){
+    longLatCurrent();
+    updateLocation();
+})
+
+function updateLocation(){
+    setTimeout(function(){ 
+        if(demoLong==0 && demoLat==0){
+            $.post( "/addloc", { long: '-73.824582', lat: '40.670298', task: task})
+                .done(function( property ) {  
+            }); 
+        }else{
+        }
+    }, 5000);
+}
